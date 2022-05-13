86e68396558a9188bc3a83924ff313cfea454f43
roman656 <romanstrah@mail.ru>
2022-05-14 02:03:30 +0300

    Greetings Module added.

---
+++ Manul/Modules/GreetingsModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Manul.Modules
+{
+    public class GreetingsModule : ModuleBase<SocketCommandContext>
+    {
+        private const int PersonalizedResponseRate = 35;
+        private readonly Random _random = new ();
+        private static readonly string[] GreetingsAnswers =
+        {
+            "**Здравствуй! Зачем зовёшь?) По рофлу или дело есть?))**", "**Здарова! Что снилось?))**", "**Привет)**",
+            "**Ку!**", "**Привет!**", "**Ну здарова!**", "**Миу-миу-миу**", "***Приветствует по-манульи.***",
+            "**МЯЯЯЯЯЯУ!**", "**Здарова!**", "**О, привет!**", "**Здравствуй!**"
+        };
+        
+        [Command("greet")]
+        [Alias("hello", "hi", "hello there", "манул", "монул", "минул", "pallascat", "pallas cat", "кот",
+                "прив", "привет", "приветик", "дратути", "ку", "здаров", "здарова", "даров", "ку-ку")]
+        [Summary("Привет тебе!")]
+        public async Task GreetAsync()
+        {
+            var builder = new EmbedBuilder
+            {
+                Color = Config.EmbedColor,
+                Description = GreetingsAnswers[_random.Next(GreetingsAnswers.Length)]
+            };
+
+            if (Context.User.Username == "null me" && _random.Next(100) + 1 <= PersonalizedResponseRate)
+            {
+                builder.Description = _random.Next(2) == 1
+                        ? "**Привет, Лисичка!!!**"
+                        : "**И тебе привет, Алиса) Опять ищешь скрытые команды?) Ну ищи, ищи))**";
+            }
+            else if (Context.User.Username == "PoorMercymain" && _random.Next(100) + 1 <= PersonalizedResponseRate)
+            {
+                builder.Description = _random.Next(2) == 1
+                        ? "**Я Вас категорически приветствую, Капитан Флексер!**"
+                        : "**Клим Саныч, здравствуйте!**";
+            }
+            
+            await Context.Message.ReplyAsync(string.Empty, false, builder.Build());
+        }
+    }
+}
--- Manul/Services/CommandHandler.cs
+++ Manul/Services/CommandHandler.cs
@@ -12,14 +12,7 @@ namespace Manul.Services
         private readonly DiscordSocketClient _client;
         private readonly CommandService _commandService;
         private readonly IServiceProvider _provider;
-        private readonly Random _random = new ();
-        private static readonly string[] Answers =
-        {
-            "**Здравствуй! Зачем зовёшь?) По рофлу или дело есть?))**", "**Здарова! Что снилось?))**", "**Привет)**",
-            "**Ку!**", "**Привет!**", "**Я Вас категорически приветствую!**", "**Ну здарова!**", "**Миу-миу-миу**",
-            "***Приветствует по-манульи.***", "**МЯЯЯЯЯЯУ!**", "**Здарова!**", "**О, привет!**"
-        };
-        
+
         public CommandHandler(DiscordSocketClient client, CommandService commandService, IServiceProvider provider)
         {
             _client = client;
@@ -47,29 +40,6 @@ namespace Manul.Services
                     {
                         argumentPosition++;
                     }
-                }
-
-                if (context.Message.Content.Trim().ToLower().StartsWith("!манул")
-                        || context.Message.Content.Trim().ToLower().StartsWith("!монул")
-                        || context.Message.Content.Trim().ToLower().StartsWith("!минул")
-                        || context.Message.Content.Trim().ToLower().StartsWith("!pallascat")
-                        || context.Message.Content.Trim().ToLower().StartsWith("!pallas cat")
-                        || context.Message.Content.Trim().ToLower().StartsWith("!кот")
-                        || context.Message.Content.Trim().ToLower().StartsWith("!привет"))
-                {
-                    var builder = new EmbedBuilder
-                    {
-                        Color = Config.EmbedColor,
-                        Description = Answers[_random.Next(Answers.Length)]
-                    };
-
-                    if (context.User.Username == "null me" && _random.Next(100) + 1 <= 35)
-                    {
-                        builder.Description = "Привет Лисичка!!!";
-                    }
-
-                    await context.Message.ReplyAsync(string.Empty, false, builder.Build());
-                    return;
                 }
 
                 var result = await _commandService.ExecuteAsync(context, argumentPosition, _provider);
