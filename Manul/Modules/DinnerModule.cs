1ff637a6641a06f240ef787b176c4e9037fae287
roman656 <romanstrah@mail.ru>
2022-12-22 21:37:43 +0300

    Merge additions fix.

--- Manul/Modules/DinnerModule.cs
+++ Manul/Modules/DinnerModule.cs
@@ -1,5 +1,7 @@
 ﻿namespace Manul.Modules;
 
+using System.Collections.Generic;
+using System.Linq;
 using System;
 using System.Threading.Tasks;
 using Discord;
@@ -8,40 +10,26 @@ using Discord.Commands;
 public class DinnerModule : ModuleBase<SocketCommandContext>
 {
     private readonly Random _random = new ();
-    private readonly string[] _drinks =
+    private readonly Dictionary<string, string[]> _menu = new()
     {
-        "квас", "морс", "березовый сок", "питень", "тархун", "водичка колодезная если повезет"
-    };
-    
-    private readonly string[] _blins =
-    {
-        "блинчик с клубничным вареньем", "блин фермер", "блин цезарь", "блин морской богатырь", 
-        "блин с яблоками и карамелью", "блин мясной богатырь", "блин двойной с икрой", "блин с икрой"
-    };
-    
-    private readonly string[] _salats =
-    {
-        "салат витаминчик", "винегрет", "не помню я"
-    };
-    
-    private readonly string[] _soups =
-    {
-        "суп гороховый", "борщ", "уха (вроде)", "лень вспоминать"
-    };
-    
-    private readonly string[] _mainCourses =
-    {
-        "картофель по-фермерски", "картофель постный", "гречка постная", "гречка", "пюре", "пельмени"
+        { "Фирменные напитки", new[] { "Квас", "Морс", "Сок берёзовый", "Питень", "Тархун", "Узвар боярышник", "Узвар груша-дичка", "Узвар шиповник", "Квас Белый Смородина и Мята", "Квас Бородинский" } },
+        { "Блины сладкие", new[] { "Блин с шоколадным кремом", "Блин Ватрушка", "Блин с шоколадным кремом и бананом", "Блин Сгущёнка", "Блин яблочно-карамельный", "Блинчик с вишнёвым вареньем", "Блинчик с клубничным вареньем" } },
+        { "Салаты", new[] { "Салат \"Оливье\"", "Салат \"Оливье\" постный", "Салат \"Русский Цезарь\"", "Салат Винегрет постный", "Салат Винегрет с сёмгой", "Салат Витаминчик с орешками", "Сельдь под шубой" } },
+        { "Супы", new[] { "Борщ \"Теремковский\"", "Борщ постный с булочкой", "Лапша куриная с фрикадельками", "Суп гороховый постный с булочкой", "Суп гороховый с копчёностями", "Сырный крем-суп", "Уха из форели по-фински" } },
+        { "Хлеб", new[] { "Булочка пшеничная", "Булочка ржаная", "Чипы блинные в уголке" } }
     };
 
-    [Command("dinner"), Alias("теремок", "ням", "обед", "меню", "кушать", "еда", "eat",
-            "teremok", "есть", "покушать", "хрум", "food", "жрать")]
-    [Summary("выберу за тебя еду из теремка)")]
+    [Command("dinner"), Alias("теремок", "обед", "меню", "кушать", "еда", "eat", "teremok", "есть",
+         "трапеза", "food", "ужин", "menu")]
+    [Summary("что будете заказывать?")]
     public async Task DinnerAsync()
     {
         var builder = new EmbedBuilder { Color = Config.EmbedColor };
+        var brandedDrinksChoice = _menu.ElementAt(0).Value[_random.Next(_menu.ElementAt(0).Value.Length)];
+        var sweetPancakesChoice = _menu.ElementAt(1).Value[_random.Next(_menu.ElementAt(1).Value.Length)];
+        var saladsChoice = _menu.ElementAt(2).Value[_random.Next(_menu.ElementAt(2).Value.Length)];
         
-        builder.Description = $"**Твое сегодняшнее меню: {_mainCourses[_random.Next(_mainCourses.Length)]}, {_soups[_random.Next(_soups.Length)]}, {_salats[_random.Next(_salats.Length)]}, {_blins[_random.Next(_blins.Length)]}, {_drinks[_random.Next(_drinks.Length)]}**";
+        builder.Description = $"**Мои рекомендации таковы:\n *— {saladsChoice}\n — {sweetPancakesChoice}\n — {brandedDrinksChoice}***";
 
         await Context.Message.ReplyAsync(string.Empty, false, builder.Build());
     }
