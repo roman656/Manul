4e5fdc0a0c31fe8f13b68b9f482449c2110a8cc6
roman656 <romanstrah@mail.ru>
2022-12-31 01:37:33 +0300

    Added triggered avatars

--- Manul/Config.cs
+++ Manul/Config.cs
@@ -7,7 +7,7 @@ public class Config
     public const string ConfigFilename = "Config.json";
     public const string VotingDataFilename = "Voting.json";
     public const int MessageCacheSize = 500;
-    public static readonly Color EmbedColor = 0xff9900;
+    public static readonly Color EmbedColor = 0xFF9900;
     public const string Prefix = "!";
     public const string GameActivityName = "!help";
     public string Token { get; set; } = string.Empty;
